e08b24afabf11ea286369934f3be98403291e0ce
22050019-张宇鑫 <zyx1679607834@gmail.com>
2023-03-11 08:26:49 +0800

    增加了pc暂停逻辑，对于reg先写后读问题，现在没有缺少寄存器打排，五级流水时候在实现，目前已经清理好了多周期的支持

--- npc/vsrc/ysyx_22050019_IFU.v
+++ npc/vsrc/ysyx_22050019_IFU.v
@@ -15,7 +15,7 @@ module ysyx_22050019_IFU#(
     //input                 m_axi_arready     ,
     //output                m_axi_arvalid     ,
 
-    output  [63:0]        inst_addr_reg_o   , //送出去看指令的地址
+    output reg [63:0]       inst_addr   , //送出去看指令的地址
     //input                 m_axi_arready     ,
     //output                m_axi_arvalid     ,
     
@@ -24,26 +24,31 @@ module ysyx_22050019_IFU#(
     output  [31:0]        inst_o
 );
 //=========================
-// pc 计数器
-wire [63:0]               inst_addr;
-reg [63:0]  q;
-always @(posedge clk) begin
-    if (rst_n )
-        q <= RESET_VAL ;
-    else
-        q <= inst_addr+64'd4    ;   
-end
 
-assign inst_addr = inst_j?snpc:q;
+//=========================
+// pc 计数器
+always @ (posedge clk) begin
+    // 复位
+    if (rst_n) begin
+        inst_addr <= RESET_VAL;
+    // 跳转
+    end else if (inst_j) begin
+        inst_addr <= snpc;
+    // 暂停
+    end else if (0) begin
+        inst_addr <= inst_addr;
+    // 地址加4
+    end else begin
+        inst_addr <= inst_addr + 64'h4;
+    end
+end
 //=========================
 
 //IFU第一级取指令流水操作
-ysyx_22050019_Reg #(32,32'b0) i0 (clk,rst_n,inst_i,inst_o,1'b1);
-ysyx_22050019_Reg #(64,64'b0) i1 (clk,rst_n,inst_addr,inst_addr_o,1'b1);
+//ysyx_22050019_Reg #(32,32'b0) i0 (clk,rst_n,inst_i,inst_o,1'b1);
+//ysyx_22050019_Reg #(64,64'b0) i1 (clk,rst_n,inst_addr,inst_addr_o,1'b1);
 
-//assign inst_addr_o = inst_addr;
-//assign inst_o      = inst_i;
-
-assign inst_addr_reg_o = inst_addr;
+assign inst_addr_o = inst_addr;
+assign inst_o      = inst_i;
 
 endmodule
