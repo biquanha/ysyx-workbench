9619a49afa35013fc427e664917d9d9fd59db064
22050019-张宇鑫 <zyx1679607834@gmail.com>
2023-07-16 13:38:37 +0800

    11

--- am-kernels/tests/cpu-tests/tests/csr.c
+++ am-kernels/tests/cpu-tests/tests/csr.c
@@ -6,8 +6,8 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case(EVENT_YIELD) : printf("EVENT_YIELD\n"); break;
-    case(EVENT_SYSCALL) : do_syscall(c); break;
-    default: panic("Unhandled event ID = %d", e.event);
+    case(EVENT_SYSCALL) :break;
+    default: printf("Unhandled event ID = %d", e.event);assert(0);
   }
 
   return c;
@@ -38,3 +38,4 @@ int main()
 
     return 0;
 }
+
